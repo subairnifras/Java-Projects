a7cc350de80bc9f4e5d994539a46e63b55806273
subairnifras <mohamednifras508@gmail.com>
2025-02-09 21:07:04 +0530

    only added variables

---
+++ banking.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class banking {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Your User Name : ");
+        String a = scan.nextLine();
+        System.out.print("Enter Your Password : ");
+        String b = scan.nextLine();
+
+        if(a`)
+    }
+}
