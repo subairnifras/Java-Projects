dfecc96072a4d9af8f7ea57a32e859d0e34ca021
subairnifras <mohamednifras508@gmail.com>
2025-02-09 23:42:06 +0530

    first step

--- banking.java
+++ banking.java
@@ -10,6 +10,33 @@ public class banking {
         System.out.print("Enter Your Password : ");
         String b = scan.nextLine();
 
-        if(a`)
+        int nifras = 5000000;
+
+        if(a.equals("nifras") && b.equals("5549134@asd")){
+            System.out.println("***** Welcome... Nifras...! *****" + "\n   A/C No : 8528351" + "\n   A/C Type : Savings" + "\n   Balance : " + nifras + "$" + "\n1.Deposite" + "\n2.Withdrawals" + "\n3.Balance Inquery");
+        }
+
+        else{
+            System.out.print("sdfsgf");
+        }
+
+        System.out.print("Enter a Section : ");
+        int section = scan.nextInt();
+
+        if(section == 1){
+            System.out.print("Enter Amount : ");
+            int amount = scan.nextInt();
+            int deposite = nifras + amount;
+            System.out.print("Your Balance is : " +  deposite + "$");
+        }
+
+        else if(section == 2){
+            System.out.print("Enter Amount : ");
+        }
+
+        else if(section == 3){
+            System.out.print("Your Balance Amount is : " + nifras + "$");
+        }
+
     }
 }
