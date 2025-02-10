5430da245d6354c131828151aa3868b2557bf101
subairnifras <mohamednifras508@gmail.com>
2025-02-10 14:15:06 +0530

    new

--- test.java
+++ test.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 class test{
     public static void main(String[] args)
     {
-        System.out.print("Hello ...! JAVA.....!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String a = scan.nextLine();
+        System.out.print("Enter Your Password: ");
+        String b = scan.nextLine();
+
+        
+        //String x = "Nifras";
+
+        if(a.equals("nifras") && b.equals("5549134@asd")){
+            System.out.print("***** Welcome... Nifras..! *****" + "\ndfvgfdg");
+        }
+
+        else{
+            System.out.print("sgrgrgrdg");
+        }
     }
 }
